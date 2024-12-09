12bcded565f524aaff62975c21ee9c6cabd149ca
rmcabrera <mateussearc@gmail.com>
2024-12-09 09:40:13 -0500

    gifs

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { GifsService } from '../../../gifs/services/gifs.service';
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent {
+
+  constructor(private gifService: GifsService){
+   
+  }
+
+  get tags() : string []{
+    return this.gifService.tagsHistory;
+  }
+
+  searchTag(valor : string) : void {
+    this.gifService.searchTag(valor);
+  }
+}
